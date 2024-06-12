7143e5de0d34dea4bb2feb342c65fe4a52b40c84
nadinsoli <161492557+nadinsoli@users.noreply.github.com>
2024-06-12 09:13:56 +0300

    index.js

--- index.js
+++ index.js
@@ -46,3 +46,62 @@ function insertionSort(arr) {
 
 let insSortArray = insertionSort(array);
 console.log("Insertion Sorted array: ", insSortArray);
+
+// Quick sort
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    
+    let pivot = arr[arr.length - 1];
+    let left = [];
+    let right = [];
+    
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let quickSortArray = quickSort(array); 
+console.log("Quick Sorted array: ", quickSortArray);
+
+// merge sort
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+    
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+let mergeSortArray = mergeSort(array);
+console.log("Merge Sorted array: ", mergeSortArray);
